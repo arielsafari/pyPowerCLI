f79da4b8d16ba420ea96707c7f1f8108835b5ed9
Madrich <madrich@cyber.org.il>
2020-10-04 01:06:56 +0300

    Add PowerCLI installation to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import re
 import setuptools
 
+from pyPowerCLI.install import install_powercli, is_installed
 
 with open(r"pyPowerCLI\__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+
+# Install PowerCLI on the computer, 
+# and validate the installation.
+install_powercli()
+
+if not is_installed():
+    raise RuntimeError("PowerCLI haven't installed correctly.")
 
 
 setuptools.setup(
