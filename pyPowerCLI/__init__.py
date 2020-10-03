ff7d511f0ae1dc8bb591cff1ee2fb142c2bbb51e
Madrich <madrich@cyber.org.il>
2020-10-04 01:06:32 +0300

    Add imports to __init__.py

--- pyPowerCLI/__init__.py
+++ pyPowerCLI/__init__.py
@@ -1,3 +1,9 @@
 
+from .install import is_installed
+from .install import install_powercli
+from .install import uninstall_powercli
+from .powershell import Powershell
+from .powershell import PowershellException
+
 
 __version__ = "0.0.1"
